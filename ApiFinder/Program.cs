cba0a0c6f8b6521f4305324b8448be15163110dc
Otar <otar_iluridze@epam.com>
2024-03-15 14:13:10 +0400

    Minor Changes

--- ApiFinder/Program.cs
+++ ApiFinder/Program.cs
@@ -14,51 +14,42 @@ namespace ApiFinder
 {
     class Program
     {
-        static string urlToTest = @"https://2nabiji.ge/en/product/ghvino-thethri-alavi-kakhuri-mshrali-294#";
-        static string logFilePath = $"network_requests_{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.txt";
-        static readonly object fileLock = new object(); // Lock object to synchronize file access
+        static readonly string urlToTest = @"https://2nabiji.ge/en/product/ghvino-thethri-alavi-kakhuri-mshrali-294#";
+        static readonly string logFilePath = $"network_requests_{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.txt";
+        static readonly object fileLock = new();
 
         static async Task Main(string[] args)
         {
-            // Set ChromeDriver path
-            ChromeOptions chromeOptions = new ChromeOptions();
+            ChromeOptions chromeOptions = new();
             chromeOptions.AddArguments("--headless");
             ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService(@"C:\Users\oilur\source\repos\EndPointFinder\ApiFinder\", "chromedriver.exe");
 
-            // Set up Chrome driver
-            using (IWebDriver driver = new ChromeDriver(chromeDriverService, chromeOptions))
+            using IWebDriver driver = new ChromeDriver(chromeDriverService, chromeOptions);
+            try
             {
-                try
+                IDevTools devTools = driver as IDevTools;
+
+                IDevToolsSession session = devTools.GetDevToolsSession();
+                DevToolsSessionDomains devToolsSession = session.GetVersionSpecificDomains<DevToolsSessionDomains>();
+
+                await devToolsSession.Network.Enable(new Network.EnableCommandSettings());
+
+                devToolsSession.Network.RequestWillBeSent += (sender, e) =>
                 {
-                    // Get DevTools
-                    IDevTools devTools = driver as IDevTools;
-
-                    // DevTools Session
-                    IDevToolsSession session = devTools.GetDevToolsSession();
-                    DevToolsSessionDomains devToolsSession = session.GetVersionSpecificDomains<DevToolsSessionDomains>();
-
-                    // Initialize Network domain
-                    await devToolsSession.Network.Enable(new Network.EnableCommandSettings());
-
-                    // Subscribe to the RequestWillBeSent event
-                    devToolsSession.Network.RequestWillBeSent += (sender, e) =>
+                    if (Regex.IsMatch(e.Request.Url, @"\bapi\b"))
                     {
-                        if (Regex.IsMatch(e.Request.Url, @"\bapi\b"))
-                        {
-                            string requestInfo = $"{DateTime.Now} - Request URL: {e.Request.Url}, Initiator URL: {e.Initiator.Url}";
-                            WriteToFile(requestInfo);
-                        }
-                    };
+                        string requestInfo = $"{DateTime.Now} - Request URL: {e.Request.Url}, Initiator URL: {e.Initiator.Url}";
+                        WriteToFile(requestInfo);
+                    }
+                };
 
-                    // Run tests
-                    NetworkInterceptionTest(devToolsSession, driver);
-                    SetAdditionalHeadersTest(devToolsSession, driver);
-                    SetUserAgentTest(devToolsSession, driver);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"An error occurred: {ex.Message}");
-                }
+                NetworkInterceptionTest(devToolsSession, driver);
+                SetAdditionalHeadersTest(devToolsSession, driver);
+                SetUserAgentTest(devToolsSession, driver);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
 
@@ -66,7 +57,7 @@ namespace ApiFinder
         {
             devToolsSession.Network.SetBlockedURLs(new Network.SetBlockedURLsCommandSettings()
             {
-                Urls = new string[] { "*://*/*.css", "*://*/*.jpg", "*://*/*.png" }
+                Urls = ["*://*/*.css", "*://*/*.jpg", "*://*/*.png"]
             });
 
             driver.Navigate().GoToUrl(urlToTest);
@@ -100,10 +91,8 @@ namespace ApiFinder
             {
                 try
                 {
-                    using (StreamWriter writer = new StreamWriter(@"C:\Users\oilur\source\repos\EndPointFinder\ApiFinder\Records\" + logFilePath, true))
-                    {
-                        writer.WriteLine(content);
-                    }
+                    using StreamWriter writer = new StreamWriter(@"C:\Users\oilur\source\repos\EndPointFinder\ApiFinder\Records\" + logFilePath, true);
+                    writer.WriteLine(content);
                 }
                 catch (Exception ex)
                 {
