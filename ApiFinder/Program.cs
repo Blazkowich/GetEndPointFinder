e2440dd75531ebec857e9a12e1aa6cdcc4a7b13f
Otar <otar_iluridze@epam.com>
2024-03-15 14:05:04 +0400

    Created Api Finder

--- ApiFinder/Program.cs
+++ ApiFinder/Program.cs
@@ -1,45 +1,115 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using DevToolsSessionDomains = OpenQA.Selenium.DevTools.V122.DevToolsSessionDomains;
+using Network = OpenQA.Selenium.DevTools.V122.Network;
+using System;
 using System.IO;
-using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-class Program
+namespace ApiFinder
 {
-    static void Main(string[] args)
+    class Program
     {
-        string url = @"https://regex101.com/";
+        static string urlToTest = @"https://2nabiji.ge/en/product/ghvino-thethri-alavi-kakhuri-mshrali-294#";
+        static string logFilePath = $"network_requests_{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.txt";
+        static readonly object fileLock = new object(); // Lock object to synchronize file access
 
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-
-        try
+        static async Task Main(string[] args)
         {
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            // Set ChromeDriver path
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--headless");
+            ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService(@"C:\Users\oilur\source\repos\EndPointFinder\ApiFinder\", "chromedriver.exe");
+
+            // Set up Chrome driver
+            using (IWebDriver driver = new ChromeDriver(chromeDriverService, chromeOptions))
             {
-                // Request URL
-                Console.WriteLine($"Request URL: {url}");
+                try
+                {
+                    // Get DevTools
+                    IDevTools devTools = driver as IDevTools;
 
-                // Request Method
-                Console.WriteLine($"Request Method: {request.Method}");
+                    // DevTools Session
+                    IDevToolsSession session = devTools.GetDevToolsSession();
+                    DevToolsSessionDomains devToolsSession = session.GetVersionSpecificDomains<DevToolsSessionDomains>();
 
-                // Status Code
-                Console.WriteLine($"Status Code: {(int)response.StatusCode} {response.StatusCode}");
+                    // Initialize Network domain
+                    await devToolsSession.Network.Enable(new Network.EnableCommandSettings());
 
-                // Remote Address
-                Console.WriteLine($"Remote Address: {response.ResponseUri.Host}:{response.ResponseUri.Port}");
+                    // Subscribe to the RequestWillBeSent event
+                    devToolsSession.Network.RequestWillBeSent += (sender, e) =>
+                    {
+                        if (Regex.IsMatch(e.Request.Url, @"\bapi\b"))
+                        {
+                            string requestInfo = $"{DateTime.Now} - Request URL: {e.Request.Url}, Initiator URL: {e.Initiator.Url}";
+                            WriteToFile(requestInfo);
+                        }
+                    };
+
+                    // Run tests
+                    NetworkInterceptionTest(devToolsSession, driver);
+                    SetAdditionalHeadersTest(devToolsSession, driver);
+                    SetUserAgentTest(devToolsSession, driver);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
             }
-
         }
-        catch (WebException ex)
+
+        static void NetworkInterceptionTest(DevToolsSessionDomains devToolsSession, IWebDriver driver)
         {
-            if (ex.Response is HttpWebResponse errorResponse)
+            devToolsSession.Network.SetBlockedURLs(new Network.SetBlockedURLsCommandSettings()
             {
-                Console.WriteLine($"Error Response Status Code: {(int)errorResponse.StatusCode} {errorResponse.StatusCode}");
-            }
-            else
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
+                Urls = new string[] { "*://*/*.css", "*://*/*.jpg", "*://*/*.png" }
+            });
+
+            driver.Navigate().GoToUrl(urlToTest);
         }
 
-        Console.ReadLine();
+        static void SetAdditionalHeadersTest(DevToolsSessionDomains devToolsSession, IWebDriver driver)
+        {
+            var extraHeader = new Network.Headers();
+            extraHeader.Add("headerName", "executeHacked");
+            devToolsSession.Network.SetExtraHTTPHeaders(new Network.SetExtraHTTPHeadersCommandSettings()
+            {
+                Headers = extraHeader
+            });
+
+            driver.Navigate().GoToUrl(urlToTest);
+        }
+
+        static void SetUserAgentTest(DevToolsSessionDomains devToolsSession, IWebDriver driver)
+        {
+            devToolsSession.Network.SetUserAgentOverride(new Network.SetUserAgentOverrideCommandSettings()
+            {
+                UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)"
+            });
+
+            driver.Navigate().GoToUrl(urlToTest);
+        }
+
+        static void WriteToFile(string content)
+        {
+            lock (fileLock)
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(@"C:\Users\oilur\source\repos\EndPointFinder\ApiFinder\Records\" + logFilePath, true))
+                    {
+                        writer.WriteLine(content);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while writing to the file: {ex.Message}");
+                }
+            }
+        }
     }
 }
